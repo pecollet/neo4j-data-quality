88eaf72653bda7f57913b5162338a052023d552f
David Pecollet <david.pecollet@gmail.com>
2020-09-28 15:06:08 +0100

    added attachment and list procs

--- src/main/java/neo4j/dq/DQ.java
+++ src/main/java/neo4j/dq/DQ.java
@@ -6,6 +6,7 @@ import org.neo4j.internal.helpers.collection.Pair;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.*;
 
+import java.time.ZonedDateTime;
 import java.util.*;
 import java.util.stream.Stream;
 import org.neo4j.graphdb.Node;
@@ -26,41 +27,85 @@ public class DQ  {
     public Log log;
 
     @Procedure(value = "neo4j.dq.createFlag", mode=Mode.WRITE)
-    @Description("")
+    @Description("creates a new Data Quality flag on the given node")
     public Stream<FlagResult> createFlag(@Name("node") Node node,
-                                         @Name(value="flagLabel", defaultValue = "DQ_Flag")  String flagLabel,
+                                         @Name(value="flagLabel", defaultValue = "Generic_Flag")  String flagLabel,
                                          @Name(value="description", defaultValue = "")  String description
             ) {
-        //todo : multiple labels, multiple nodes sharing a flag
+        //todo : description as complex map/list
         Node root;
         try {
-            root = tx.findNode(Label.label("DQ"), "name", "root");
+            root = tx.findNode(Label.label("DQ_All"), "class", "all");
             if (root == null) {
-                root= tx.createNode(Label.label("DQ"));
-                root.setProperty("name", "root");
+                root= tx.createNode(Label.label("DQ_All"), Label.label("DQ_Class"));
+                root.setProperty("class", "all");
             }
         } catch (MultipleFoundException mfe) {
-            log.error("Multiple roots found : clean up nodes with label 'DQ' named 'root'.");
+            log.error("Multiple roots found : several nodes with label 'DQ_All'.");
             return Stream.of(null);
         }
-        Node flag= tx.createNode(Label.label(flagLabel));
-        flag.createRelationshipTo(root ,RelationshipType.withName("SUBCLASS_OF"));
+        Node parent;
+        try {
+            parent = tx.findNode(Label.label("DQ_Class"), "class", flagLabel);
+            if (parent == null) {
+                parent= tx.createNode(Label.label("DQ_Class"));
+                parent.setProperty("class", flagLabel);
+                parent.setProperty("flagCount", 0);
+                parent.createRelationshipTo(root, RelationshipType.withName("HAS_DQ_CLASS"));
+            }
+        } catch (MultipleFoundException mfe) {
+            log.error("Multiple parents found : multiple 'DQ_Class' nodes with class='"+flagLabel+"'..");
+            return Stream.of(null);
+        }
+        Node flag= tx.createNode(Label.label(flagLabel), Label.label("DQ_Flag"));
+        flag.createRelationshipTo(parent, RelationshipType.withName("HAS_DQ_CLASS"));
         flag.setProperty("description", description);
-        node.createRelationshipTo(flag, RelationshipType.withName("HAS_DG_FLAG"));
+        flag.setProperty("created", ZonedDateTime.now());
+        node.createRelationshipTo(flag, RelationshipType.withName("HAS_DQ_FLAG"));
+        //todo : update counts/stats on parent. lock?
+        parent.setProperty("flagCount", (Integer)parent.getProperty("flagCount") +1);
         return Stream.of( new FlagResult(flag));
     }
 
+    @Procedure(value = "neo4j.dq.attachToFlag", mode=Mode.WRITE)
+    @Description("adds an attachment node to a Data Quality flag")
+    public Stream<FlagAttachmentResult> attachToFlag(@Name("flag") Node flag,
+                                         @Name("attachmentNode")  Node attachmentNode,
+                                         @Name(value="description", defaultValue = "")  String description
+    ) {
+        Relationship r=flag.createRelationshipTo(attachmentNode, RelationshipType.withName("HAS_ATTACHMENT"));
+        r.setProperty("description", description);
+        return Stream.of( new FlagAttachmentResult(r));
+    }
 
+    @Procedure(value="neo4j.dq.listClasses")
+    @Description("list all classes of DQ flags")
+    public Stream<ClassResult> listClasses()  {
+        ResourceIterator<Node> classNodes=tx.findNodes(Label.label("DQ_Class"));
+        return classNodes.stream().map(e -> new ClassResult(e));
+    }
 
 
     //result type
     public static class FlagResult {
         // yield
         public final Node flag;
-
-
         public FlagResult(Node node) {
             this.flag = node;
+        }
+    }
+    public static class ClassResult {
+        // yield
+        public final Node dqClass;
+        public ClassResult(Node node) {
+            this.dqClass = node;
+        }
+    }
+    public static class FlagAttachmentResult {
+        // yield
+        public final Relationship attachment;
+        public FlagAttachmentResult(Relationship rel) {
+            this.attachment = rel;
         }
     }
 }
